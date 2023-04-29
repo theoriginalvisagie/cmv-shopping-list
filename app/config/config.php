912518cb1428f4f5b3152a645dd97bad73fd762a
Christiaan Visagie <visagiechristiaan40@gmail.com>
2023-04-29 10:44:54 +0200

    Config updated

--- app/config/config.php
+++ app/config/config.php
@@ -8,7 +8,7 @@
   // App Root
   define('APPROOT', dirname(dirname(__FILE__)));
   // URL Root
-  define('URLROOT', 'http://localhost/shoppinglist');
+  define('URLROOT', 'http://localhost/cmv-shopping-list');
   // Site Name
   define('SITENAME', 'Shopping List');
   
