9cc3c37471515f344d701d88f6d33e92000cf7ae
Christiaan Visagie <visagiechristiaan40@gmail.com>
2023-04-29 13:35:04 +0200

    Jquery and sweetalert CDN added

--- app/views/includes/footer.php
+++ app/views/includes/footer.php
@@ -1,3 +1,5 @@
-</div>
-</body>
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+        <script src="https://code.jquery.com/jquery-3.6.4.js" integrity="sha256-a9jBBRygX1Bh5lt8GZjXDzyOB+bWve9EiO7tROUtj/E=" crossorigin="anonymous"></script>
+    </body>
 </html>
