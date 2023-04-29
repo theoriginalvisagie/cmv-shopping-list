dd75a2455d75e66ee4df392718b2798e5abaac4c
Christiaan Visagie <visagiechristiaan40@gmail.com>
2023-04-29 13:42:02 +0200

    Script include fixed

--- app/views/includes/footer.php
+++ app/views/includes/footer.php
@@ -1,4 +1,5 @@
         </div>
+        <script src="<?php echo URLROOT; ?>/public/js/main.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
         <script src="https://code.jquery.com/jquery-3.6.4.js" integrity="sha256-a9jBBRygX1Bh5lt8GZjXDzyOB+bWve9EiO7tROUtj/E=" crossorigin="anonymous"></script>
     </body>
