859dace0ee65091dd802a58a55db3fe7df05960e
Christiaan Visagie <visagiechristiaan40@gmail.com>
2023-04-29 12:31:14 +0200

    Code clean up and redirect to home page

--- app/controllers/Items.php
+++ app/controllers/Items.php
@@ -2,7 +2,7 @@
   class Items extends Controller{
     public function __construct(){
       // Load Models
-      $this->itemModel = $this->model('Item');
+      $this->itemModel = $this->model("Item");
     }
 
     // Load All Posts
@@ -11,48 +11,47 @@
       
 
       $data = [
-        'items' => $items
+        "items" => $items
       ];
       
-      $this->view('items/index', $data);
+      $this->view("items/index", $data);
     }
 
-    // Add Post
+    // Add Item
     public function add(){
-      if($_SERVER['REQUEST_METHOD'] == 'POST'){
+      if($_SERVER['REQUEST_METHOD'] == "POST"){
         // Sanitize POST
         $_POST  = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
         
         $data = [
-          'item' => trim($_POST['item']),
-          'qty' => trim($_POST['quantity']),  
-          'item_err' => '',
-          'qty_err' => ''
+          "item" => trim($_POST['item']),
+          "qty" => trim($_POST['quantity']),  
+          "item_err" => '',
+          "qty_err" => ""
         ];
 
          if(empty($data['item'])){
-          $data['item_err'] = 'Please enter a name';
+          $data['item_err'] = "Please enter a name";
 
           if(empty($data['quantity'])){
-            $data['qty_err'] = 'Please enter a quantity';
+            $data['qty_err'] = "Please enter a quantity";
           }
         }
 
         // Make sure there are no errors
         if(empty($data['item_err']) && empty($data['qty_err'])){
-          // Validation passed
+          //Validation passed
           //Execute
-          // print_r($data);
           if($this->itemModel->addItem($data)){
             // Redirect to login
-            flash('items_added', 'Items Added');
-            redirect('items');
+            flash("items_added", "Items Added");
+            redirect();
           } else {
-            die('Something went wrong');
+            die("Something went wrong");
           }
         } else {
           // Load view with errors
-          $this->view('items/add', $data);
+          $this->view("items/add", $data);
         }
 
       } else {
