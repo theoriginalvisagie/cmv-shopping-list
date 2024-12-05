2d34ad37cf7d9464fe9c5bde521ca371eabfe213
Christiaan Visagie <visagiechristiaan40@gmail.com>
2024-12-05 07:52:47 +0200

    Config update

--- app/config/config.php
+++ app/config/config.php
@@ -8,7 +8,7 @@
   // DB Params
   define("DB_HOST", getenv("DB_HOST"));
   define("DB_USER", getenv("DB_USER"));
-  define("DB_PASS", getenv(""));
+  define("DB_PASS", getenv("DB_PASS"));
   define("DB_NAME", getenv("DB_NAME"));
 
   // URL Root
